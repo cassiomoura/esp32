05ef9d854c3d1539fe62ff57ae2f752633499fc2
cassiomoura <89200463+cassiomoura@users.noreply.github.com>
2022-03-14 11:24:21 -0300

    Update main.py

--- ota/main.py
+++ ota/main.py
@@ -3,7 +3,7 @@ from machine import Pin
 led=Pin(2,Pin.OUT)
 
 while True:
-led.value(1)
-time.sleep(0.5)
-led.value(0)
-time.sleep(0.5)
+  led.value(1)
+  time.sleep(0.5)
+  led.value(0)
+  time.sleep(0.5)
