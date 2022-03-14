261cbdfec583c2890a5797b10c879ccb3ff476d8
cassiomoura <89200463+cassiomoura@users.noreply.github.com>
2022-03-14 11:09:00 -0300

    Update main.py

--- ota/main.py
+++ ota/main.py
@@ -2,5 +2,7 @@ from machine import Pin
 import time
 
 while True
-    Pin(2, Pin.OUT).value(not Pin2.value())
+    Pin(2, pin.OUT).value(1)
+    time.sleep(3)
+    Pin(2, pin.OUT).value(0)
     time.sleep(3)
